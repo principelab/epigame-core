259e72d89b7c61566bbbf03238cd15af681d96eb
Karla Ivankovic <39747361+ivkarla@users.noreply.github.com>
2026-02-12 17:15:00 +0100

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -49,7 +49,7 @@ score = epigame_predict_from_mat(
     fs=500,
     max_sigma=4,
     connectivity_measures = ["PAC"],
-    bands=[None,(1,4),(4,8),(8,13),(13,30),(30,70),(70,150)]
+    bands=[(1,4),(4,8),(8,13),(13,30),(30,70),(70,150)]
 )
 
 print("\nEpigame run complete.")
